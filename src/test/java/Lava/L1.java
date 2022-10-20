7067f33eca75b6916e76db99b98b5fa9386457b7
Harish246 <srihari224466@gmail.com>
2022-10-20 11:54:59 +0530

    toD

--- src/test/java/Lava/L1.java
+++ src/test/java/Lava/L1.java
@@ -5,6 +5,7 @@ public class L1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("nothing is perminent");
+		System.out.println("life moves on");
 
 	}
 
